c41979fcb63ef06d4df7ad27978f51ea4e1f19e1
Taylor Vargas Teixeira <taylorvargasteixeira@gmail.com>
2024-02-08 17:29:47 -0300

    initial app with fastify and prisma

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import fastify from "fastify"
+import { createPoll } from "./routes/create-poll"
+
+const app = fastify()
+
+app.register(createPoll)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333")
+})
